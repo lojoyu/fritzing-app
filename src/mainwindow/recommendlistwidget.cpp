595123f3effe5176eed8f417fe7d4b4650636ed8
awinder0230 <awinder0230@gmail.com>
2018-09-17 15:09:05 +0800

    tutorial nameing

--- src/mainwindow/recommendlistwidget.cpp
+++ src/mainwindow/recommendlistwidget.cpp
@@ -111,7 +111,9 @@ void RecommendListWidget::onItemEvent(QListWidgetItem* listitem, bool hover) {
             QString str = tList[i].value<QString>();
             QLabel * label = new QLabel();
             label->setAlignment(Qt::AlignLeft);
-            label->setText(QString("<a href='%1'>tutorial %2</a>").arg(str).arg(i));
+            // label->setText(QString("<a href='%1'>tutorial %2</a>").arg(str).arg(i));
+            QString title = str.split("/").last().replace("-", " ");
+            label->setText(QString("<a href='%1'>%2</a>").arg(str).arg(title));
             label->setOpenExternalLinks(true);
             //layout->addWidget(label);
             QListWidgetItem* item = new QListWidgetItem();
